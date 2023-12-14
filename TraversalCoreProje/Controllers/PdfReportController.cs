313f3b17f95924242c7330ba1e1a059be6c5f13c
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-12-14 04:46:29 +0300

    Ders 60 Sonu Pdf Raporları

---
+++ TraversalCoreProje/Controllers/PdfReportController.cs
@@ -0,0 +1,66 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raportu");
+
+            document.Add(paragraph);
+            document.Close();
+
+            return File("/pdfreprots/dosya1.pdf", "application/pdf", "dosya1.pdf");
+        }
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+            //2. olan PdfTable
+            PdfPTable pdfTable = new PdfPTable(3);
+            pdfTable.AddCell("Misafir Adı");
+            pdfTable.AddCell("Misafir Soyadı");
+            pdfTable.AddCell("Misafir TC");
+
+            pdfTable.AddCell("Eylül");
+            pdfTable.AddCell("Test");
+            pdfTable.AddCell("12345678901");
+
+            pdfTable.AddCell("Kemal");
+            pdfTable.AddCell("Yıldırım");
+            pdfTable.AddCell("12345678902");
+
+            pdfTable.AddCell("Mehmet");
+            pdfTable.AddCell("Can");
+            pdfTable.AddCell("12345678903");
+
+            document.Add(pdfTable);
+
+            document.Close();
+
+            return File("/pdfreprots/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
+    }
+}
