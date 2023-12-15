77446572fce6855572c6e4d55e214e4e9d17d158
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-12-16 00:32:25 +0300

    Ders 69 Sonu Api kullanımı

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TraversalDBApi;Integrated Security=True;");
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+
+    }
+}
