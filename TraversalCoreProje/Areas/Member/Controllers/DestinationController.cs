7f13a4b4d923ebd2bcfca518755251f3e62326da
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-12-19 20:58:26 +0300

    Ders 97 sonu menulere dil destgi ve arama islemi

--- TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -2,11 +2,11 @@
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
-    [AllowAnonymous]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
@@ -14,6 +14,16 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
         }
     }
 }
