658e44cfd7e160821212dc741f216c562d3081bc
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-12-20 00:59:21 +0300

    Ders 98 Sonu Sifremi unuttum ve reset password

---
+++ TraversalCoreProje/Controllers/PasswordChangeController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper.Internal;
+using EntityLayer.Concrete;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Threading.Tasks;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class PasswordChangeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public PasswordChangeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Mail);
+            string passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var passwordResetTokenLink = Url.Action("ResetPassword", "PasswordChange", new
+            {
+                userId = user.Id,
+                token = passwordResetToken
+            }, HttpContext.Request.Scheme);
+
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "traversalcore2@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.Mail);
+            mimeMessage.From.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = "Şifre Değişiklik Talebi";
+            //mimeMessage.Body = mailRequest.Body; bu şekilde hata verir alttaki şekilde kullanılmali bir entity olmalı string değil
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = passwordResetTokenLink;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            //Gmail in güvenlik sçeeneğine gel 2 adımlı dogrulamayı açman gerek google un yeni mail gönderme işlemelrine göre bu yapılmalı tekrardan myaccount google.com a gel uygulama şifreleri kısmına gel bir uygulama ve cihazı seç isim ver ve verdiği şifre yi uygulama şifresini yaz 3. parti yazılımlardan mail gönderme işlemi bu şekilde yapılır gmail de
+            client.Authenticate("traversalcore2@gmail.com", "gmail_uygulama_şifresi");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string userid, string token)
+        {
+            TempData["userid"] = userid;
+            TempData["token"] = token;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            var userid = TempData["userid"];
+            var token = TempData["token"];
+            if (userid == null || token == null)
+            {
+                //hata mesajı
+            }
+            var user = await _userManager.FindByIdAsync(userid.ToString());
+            var result = await _userManager.ResetPasswordAsync(user, token.ToString(), model.Password);
+            if (result.Succeeded)
+                return RedirectToAction("SignIn", "Login");
+            return View();
+        }
+
+    }
+}
